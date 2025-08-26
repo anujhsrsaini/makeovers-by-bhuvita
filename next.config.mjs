08067dd7a260d9a897ecec2f586735a05455791a
Anuj Saini <anujhsrsaini@gmail.com>
2025-08-26 17:47:59 +0530

    Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/makeovers-by-bhuvita', // Replace with your repository name
+    assetPrefix: '/makeovers-by-bhuvita/', // Replace with your repository name
+  }
+  
+  module.exports = nextConfig
